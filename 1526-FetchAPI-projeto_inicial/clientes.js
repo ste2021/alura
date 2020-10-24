c8a5af034f1bb6e06acc8b68427e702743aafa53
ste2021 <estela.oliveira.cont@outlook.com>
2020-10-24 14:53:35 -0300

    Aprendendo Filter

---
+++ 1526-FetchAPI-projeto_inicial/clientes.js
@@ -0,0 +1,16 @@
+const informacoesClientes = [ //a gnt cria um objeto, esse objeto é feito dentro das chaves
+    {
+        cpf: 66809433023, //quando é array, é separado por virgula
+        nome: "Jairo"
+
+    }
+];
+
+const conteudoLinha = `
+<tr>
+<td>66809433023</td>
+<td>Jairo</td>
+<td></td>
+</tr>`
+
+console.log(conteudoLinha);
