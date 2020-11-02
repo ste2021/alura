a5bf9654c4154ec97796f6bd806e6fbf275bbf51
ste2021 <estela.oliveira.cont@outlook.com>
2020-11-02 12:14:07 -0300

    adivinhacao quebrando loop

---
+++ JS2/introducao-javascript/js/main.js
@@ -0,0 +1,12 @@
+// var titulo = document.querySelector(".titulo");
+// titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+
+console.log(tdPeso); // contem o tr
+console.log(paciente)
+
+// var peso = tdPeso.textContent;
+
+// var tdAltura = paciente.querySelector(".info-peso");
