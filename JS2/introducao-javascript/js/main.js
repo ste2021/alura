ea1097d3233d94d57efbaef9f67b3143b55c944e
ste2021 <estela.oliveira.cont@outlook.com>
2020-11-03 17:18:41 -0300

    js2

--- JS2/introducao-javascript/js/main.js
+++ JS2/introducao-javascript/js/main.js
@@ -1,22 +1,51 @@
-var paciente = document.querySelector("#primeiro-paciente");
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+//propriedade length para saber o tamanho doa array, podemos fazer um console.log(pacientes.length)
+//pr percorerr o array de 0 a 5, i é só uma sintaxe
+for (i = 0; i <= pacientes.length; i++) {
 
-if (peso < 0 || peso > 1000) { //as duas barrinhas siginifcam e, ou, em js
-    console.log("Peso Invalido")
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if (peso <= 0 || peso >= 1000) { //as duas barrinhas siginifcam e, ou, em js
+        console.log("Peso INvalido");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso Inválido"
+        paciente.classList.add("paciente-invalido");
+    }
+    if (altura <= 0 || altura >= 3.00) { //as duas barrinhas siginifcam e, ou, em js
+        console.log("Altura INvalido");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura INVÁILIDA"
+        paciente.classList.add("paciente-invalido");
+    }
+    if (alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    } else {
+        tdImc.textContent = "Altura e/ou peso inválidos!"
+    }
+
+
 }
+titulo.addEventListener("click", mostraMensagem);
 
-var tdImc = paciente.querySelector(".info-imc");
-
-var imc = peso / (altura * altura);
-
-tdImc.textContent = imc;
-
-console.log(imc);
-// var peso = tdPeso.textContent;
+function mostraMensagem() {
+    console.log("Olá eu fui clicado!");
+}
 
 // var tdAltura = paciente.querySelector(".info-peso");
