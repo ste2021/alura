03bcafcfd6390ba21735a6074a139f34aed82aa4
ste2021 <estela.oliveira.cont@outlook.com>
2020-11-03 13:19:17 -0300

    js2

--- JS2/introducao-javascript/js/main.js
+++ JS2/introducao-javascript/js/main.js
@@ -1,12 +1,22 @@
-// var titulo = document.querySelector(".titulo");
-// titulo.textContent = "Aparecida Nutricionista";
-
 var paciente = document.querySelector("#primeiro-paciente");
+
 var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
 
-console.log(tdPeso); // contem o tr
-console.log(paciente)
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
 
+if (peso < 0 || peso > 1000) { //as duas barrinhas siginifcam e, ou, em js
+    console.log("Peso Invalido")
+}
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var imc = peso / (altura * altura);
+
+tdImc.textContent = imc;
+
+console.log(imc);
 // var peso = tdPeso.textContent;
 
 // var tdAltura = paciente.querySelector(".info-peso");
