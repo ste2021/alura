fb6d87f8e1ee2cf130002050f1de19783fcd5c1c
ste2021 <estela.oliveira.cont@outlook.com>
2020-11-07 17:18:16 -0300

    estudos FETCH

--- FETCH_API/api/cliente.js
+++ FETCH_API/api/cliente.js
@@ -1,20 +1,63 @@
-const url = "http://localhost:4000/clientes"
-const url2 = "http://localhost:4000/clientes/cliente"
-
+const listarClientes = () => {
+  return fetch("http:localhost:4000/clientes", {
+    method: "get"
+  })
+    .then(resp => {
+      return resp.json();
+    })
+    .then(json => {
+      return json;
+    });
+};
 
 const cadastrarClientes = (nome, cpf) => {
-    const json = JSON.stringify({
-        nome: nome,
-        cpf: cpf
+  const json = JSON.stringify({
+    nome: nome,
+    cpf: cpf
+  });
+
+  return fetch("http:localhost:4000/clientes/cliente", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: json
+  }).then(resp => {
+    
+      return resp.body;
+    } 
+  );
+}
+
+const deletaCliente = id => {
+  return fetch(`http:localhost:4000/clientes/cliente/${id}`, {
+    method: "DELETE",
+})}
+
+const detalhaCliente = id => { 
+  return fetch(`http:localhost:4000/clientes/cliente/${id}`, {
+    method: "GET"
+  })
+    .then(resposta => {
+      return resposta.json();
     })
-    return fetch(url2, {
-            method: "POST",
-            headers: {
-                "Content.type": "aplication/json"
-            },
-            body: json
-        })
-        .then(resp => {
-            return resp.body //resposta q vai ser enviada ao servidor
-        });
+}
+
+const editaCliente = (id, cpf, nome) => {
+  const json = JSON.stringify({
+    nome: nome,
+    cpf: cpf
+  });
+
+  return fetch(`http:localhost:4000/clientes/cliente/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: json
+  }).then(resp => {
+    
+      return resp.body;
+    } 
+  );
 }
--- FETCH_API/listagem-cliente.js
+++ FETCH_API/listagem-cliente.js
@@ -1,24 +1,31 @@
-//no html coloca do lado elemento data-conteudo-tabela, pelo query selector o js pega o conteudo html, 
-//queremos colocar os dados da api na tabela, quem pega esses dados é innerHTML
-const corpoTabela = document.querySelector("[data-conteudo-tabela]")
-
-
-
-const exibeCliente = (cpf, nome) => { //arrowfunction, pegou os dados do conteudoLinha
-    const linha = document.createElement('tr') //propriedade createElement cria a tr na tabela
-    const conteudoLinha = `
-     <td>${cpf}</td>    
-    <td>${nome}</td>
-    `
-        //template strings, n precisam colocar o nome e cpf, ele busca no array
-    linha.innerHTML = conteudoLinha; //propriedade que exibe no html
-    return linha; //chama a linha pr criar a tabela
+const removeCliente = id => {
+  if(confirm("Deseja deletar o cliente ?"))
+  deletaCliente(id)
+  document.location.reload();
 }
+const exibeCliente = (cpf, nome, id) => {
+  const linha = document.createElement("tr");
 
-listarClientes().then(exibe => {
-        exibe.forEach(indice => { ///posicao array indidce faz esse papel p poder percorrer
-            corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome))
-        })
-    }
+  const conteudoLinha = ` 
+      
+      <td>${cpf}</td>
+      <td>${nome}</td>
+      <button type="button" class="btn btn-danger"
+      onclick="removeCliente(${id})">Excluir</button>
+      <a href="edita-clientes.html?id=${id}">
+        <button type="button" class="btn btn-info">Editar</button>
+      </a>
+  `;
 
-)
+  linha.innerHTML = conteudoLinha;
+
+  return linha;
+};
+
+const corpoTabela = document.querySelector("[data-conteudo-tabela]");
+
+listarClientes().then(clientes => {
+  clientes.forEach(indice => {
+    corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome, indice.id));
+  });
+});
