e58ffe5fdcaf227a95b0507bc63b0b7fdbd4a774
ste2021 <estela.oliveira.cont@outlook.com>
2020-11-16 09:09:43 -0300

    JSOO Constructor

--- JSOO/ContaCorrente.js
+++ JSOO/ContaCorrente.js
@@ -1,13 +1,37 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente{
     agencia;
-     // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
-    _saldo = 0;
+    _cliente;
+       // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
+       _saldo = 0;
+
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+    get saldo(){
+        return this._saldo;
+    }
+    constructor(cliente,agencia){
+        this.agencia = agencia;
+        this.cliente = cliente;
+    }  
+
 
     sacar(valor){
         if(this._saldo >= valor){
             this._saldo -= valor;
             return valor;
         }
+
+    
     }
 
     depositar(valor){
@@ -16,5 +40,12 @@ export class ContaCorrente{
             return;
         } 
         this._saldo += valor;           
+    }
+
+    tranferir(valor, conta){
+        
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+        
     }
 }
--- JSOO/index.js
+++ JSOO/index.js
@@ -1,23 +1,16 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
-const cliente1 = new Cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
+const cliente1 = new Cliente("Ricadro",  11122233309);
+const cliente2 = new Cliente("Alice", 88822233309 );
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+contaCorrenteRicardo.depositar(500);
+const conta2 = new ContaCorrente(102, cliente2);
+
+let valor = 200;
+contaCorrenteRicardo.tranferir(valor, conta2);
+
+console.log(cliente2);
 
 
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
-
-contaCorrenteRicardo.depositar(-100);
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
-
-const valorSacado = contaCorrenteRicardo.sacar(50);
-console.log(valorSacado);
-
-console.log(contaCorrenteRicardo);
