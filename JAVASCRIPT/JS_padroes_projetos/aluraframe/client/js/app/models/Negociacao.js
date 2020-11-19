a9c2521719a20a0de546b557fb414c7a0404cfa3
ste2021 <estela.oliveira.cont@outlook.com>
2020-11-19 13:54:15 -0300

    controller

--- JAVASCRIPT/JS_padroes_projetos/aluraframe/client/js/app/models/Negociacao.js
+++ JAVASCRIPT/JS_padroes_projetos/aluraframe/client/js/app/models/Negociacao.js
@@ -2,7 +2,7 @@ class Negociacao {
 
     constructor(data, quantidade, valor){
 
-        this._data = data;
+        this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
         
@@ -14,7 +14,7 @@ class Negociacao {
     }
 ///esses métodos podem acessar os atributos privados __*
          get data(){
-             return this._data;
+             return new Date(this._data.getTime());
          }
          get quantidade(){
              return this._quantidade;
