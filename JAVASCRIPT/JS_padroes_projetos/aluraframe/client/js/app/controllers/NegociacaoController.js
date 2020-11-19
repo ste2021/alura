09060572c2dbc66eddd5d990d79295731f6de0a8
ste2021 <estela.oliveira.cont@outlook.com>
2020-11-19 17:43:07 -0300

    TYPESCRIPT1

--- JAVASCRIPT/JS_padroes_projetos/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ JAVASCRIPT/JS_padroes_projetos/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,29 +1,26 @@
 class NegociacaoController {
-      
-    constructor(){
-        let $ = document.querySelector.bind(document);//metodo bind atribui o document ao query selector, permitindo colocar ele numa variavel
+
+    constructor() {
+        let $ = document.querySelector.bind(document); //metodo bind atribui o document ao query selector, permitindo colocar ele numa variavel
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
     }
 
     adiciona(event) {
-
         event.preventDefault();
-       
-        let data = new Date(
-            ...this._inputData.value
-            .split('-')
-            .map((item, indice) => item - indice % 2 )            
-            ); 
 
-            let negociacao = new Negociacao(
-                data,
-                this._inputQuantidade.value,
-                this._inputValor.value
-            );
-            console.log(negociacao);
-        } 
-      
+        
+
+        let negociacao = new Negociacao(
+            DateHelper.textoParaData(this._inputData.value), 
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negociacao);
+
+        console.log(DateHelper.dataParaTexto(negociacao.data));
     }
 
+}
