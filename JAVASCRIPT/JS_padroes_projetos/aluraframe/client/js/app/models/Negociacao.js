84b60ab76a0cd78a5b1a988ab2ec2187c68a4d01
ste2021 <estela.oliveira.cont@outlook.com>
2020-12-04 11:52:20 -0300

    finalizandoMOduloPadroesProjetos

--- JAVASCRIPT/JS_padroes_projetos/aluraframe/client/js/app/models/Negociacao.js
+++ JAVASCRIPT/JS_padroes_projetos/aluraframe/client/js/app/models/Negociacao.js
@@ -1,25 +1,30 @@
 class Negociacao {
-
-    constructor(data, quantidade, valor){
-
+    
+    constructor(data, quantidade, valor) {
+        
         this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
-        
         Object.freeze(this);
     }
-    //funcção dentro de classe é chamada de método
-         get volume(){
-         return this._quantidade * this._valor;
+    
+    get volume() {
+        
+        return this._quantidade * this._valor;
     }
-///esses métodos podem acessar os atributos privados __*
-         get data(){
-             return new Date(this._data.getTime());
-         }
-         get quantidade(){
-             return this._quantidade;
-         }
-         get valor(){
-             return this._valor;
-         }
+    
+    get data() {
+        
+        return new Date(this._data.getTime());
+    }
+    
+    get quantidade() {
+        
+        return this._quantidade;
+    }
+    
+    get valor() {
+        
+        return this._valor;
+    }
 }
